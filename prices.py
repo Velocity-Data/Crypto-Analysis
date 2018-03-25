e43a834f036cdd01a25a52cdc2960da6c9a531f9
Velocity-Data <37776213+Velocity-Data@users.noreply.github.com>
2018-03-25 11:12:49 -0400

    Add files via upload
    What has been done so far....

---
+++ prices.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+import requests
+from time import gmtime, strftime, sleep
+import sqlite3
+
+
+monero_prices = {}
+prices = []
+times = []
+
+
+def createSQLdb():
+	conn = sqlite3.connect('monero.db')
+	c = conn.cursor()
+	c.execute('''CREATE TABLE historicaldata
+             (date text, open real, high real, low real, close real)''')
+	conn.commit()
+	conn.close()
+
+def runningPrice():
+	while True:
+		curprice = {}
+		r = requests.get('https://coinmarketcap.com/currencies/monero/')
+		soup = BeautifulSoup(r.content, 'lxml')
+		for i in soup.find_all('span', {'id': 'quote_price'}):
+			price = i.contents[1].text
+			time = strftime("%Y-%m-%d %H:%M:%S")
+			curprice['Time'] = time
+			curprice['Price'] = price
+			print('Time: ', curprice['Time'], 'Price: ', curprice['Price'])
+			monero_prices = dict(zip(times, prices))
+			sleep(1)
+
+
+def historicalData():
+	data = {}
+	r = requests.get('https://coinmarketcap.com/currencies/monero/historical-data/')
+	soup = BeautifulSoup(r.content, 'lxml')
+	value = lambda td: td.attrs.get('data-format-value', td.text)
+	for row in soup.select('table.table tbody tr'):
+		data = ([value(td) for td in row.select('td')])
+		for i in data:
+			print(data)
+			
+
+
+
+historicalData()
+runningPrice()
